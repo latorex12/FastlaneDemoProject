41bcc74cbb4789c6717180b3282123846a7f4b15
SkyRim <latorex_12@126.com>
2018-01-22 14:16:17 +0800

    initialize

---
+++ FastlaneDemoTests/FastlaneDemoTests.swift
@@ -0,0 +1,35 @@
+//
+//  FastlaneDemoTests.swift
+//  FastlaneDemoTests
+//
+//  Created by SkyRim on 2018/1/22.
+//  Copyright © 2018年 SkyRim. All rights reserved.
+//
+
+import XCTest
+@testable import FastlaneDemo
+
+class FastlaneDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssert(true, "test failed")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
